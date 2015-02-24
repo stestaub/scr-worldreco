da61ca3f9cee8cd47b77263977bdf0c63d0e2660
Stefan Staub <stub@zhaw.ch>
2015-02-24 13:16:40 +0100

    fixed validation and added a time table

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -2,8 +2,8 @@ class Registration < ActiveRecord::Base
   validate :time_slots_not_same
   validates :email, :phonenumber, :first_name, :last_name, presence: true
   validates_uniqueness_of :email, scope: [:first_name, :last_name], message: "Sie scheinen sich bereits angemeldet zu haben"
-  validates :confirm_age, acceptance: true #{ accept: 1, message: "Sie müssen mindestens 18 sein um Teilnehmen zu können." }
-  validates :accept_agb, acceptance: true #{ accept: 1, message: "Bitte lesen und akzeptieren sie die Teilnahmebedingungen" }
+  validates :confirm_age, acceptance: { accept: true, message: "Sie müssen mindestens 18 sein um Teilnehmen zu können." }
+  validates :accept_agb, acceptance: { accept: true, message: "Bitte lesen und akzeptieren sie die Teilnahmebedingungen" }
 
   def time_slots_not_same
     if self.timeslot1 == self.timeslot2 ||

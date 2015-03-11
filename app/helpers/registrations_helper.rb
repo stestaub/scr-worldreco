f3ff47113c0c0f4dab1d921ca467ef1c107ae613
Stefan Staub <stub@zhaw.ch>
2015-03-11 18:08:49 +0100

    bootstrapped colorizing registration count table

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -1,2 +1,19 @@
 module RegistrationsHelper
+
+  def time_slot_color(regCount)
+    full = '#ff0000'
+    half = '#0000ff'
+    empty = '#ffffff'
+
+    limit = 2
+
+    if regCount >= limit
+      color = full
+    elsif regCount >= limit/2
+      color = half
+    else
+      color = empty
+    end
+    color
+  end
 end

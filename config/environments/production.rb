4ee18fd48eee22f4aa6cef42ea67c25008b1a536
Stefan Staub <stub@zhaw.ch>
2015-06-18 01:00:25 +0200

    added host for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,8 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
+  config.action_mailer.default_url_options = { host: 'https://limitless-spire-5397.herokuapp.com' }
+
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 

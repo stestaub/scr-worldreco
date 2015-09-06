ee025500933e50b4eafbff25328f3c1086977a70
Stefan Staub <stub@zhaw.ch>
2015-09-06 22:03:54 +0200

    fixed problem with participant form in admin pane

--- app/admin/participant.rb
+++ app/admin/participant.rb
@@ -18,5 +18,16 @@ ActiveAdmin.register Participant do
   #   permitted
   # end
 
+  form do |f|
+    f.inputs "Admin" do
+      f.input :email
+      f.input :first_name
+      f.input :last_name
+      f.input :timeslot1
+      f.input :phonenumber
+    end
+    f.actions
+  end
+
 
 end
